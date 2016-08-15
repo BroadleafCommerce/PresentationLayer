caeb787e8b8aa212dc347fee6eeecf6c150aea2a
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2016-08-15 17:49:29 -0500

    Removed unecessary xml and moved processor spring config into java config since some more complicated things had to happen

--- broadleaf-thymeleaf2-presentation/src/main/java/org/broadleafcommerce/common/config/Thymeleaf2Config.java
+++ broadleaf-thymeleaf2-presentation/src/main/java/org/broadleafcommerce/common/config/Thymeleaf2Config.java
@@ -1,10 +1,30 @@
+/*
+ * #%L
+ * broadleaf-thymeleaf2-presentation
+ * %%
+ * Copyright (C) 2009 - 2016 Broadleaf Commerce
+ * %%
+ * Licensed under the Broadleaf Fair Use License Agreement, Version 1.0
+ * (the "Fair Use License" located  at http://license.broadleafcommerce.org/fair_use_license-1.0.txt)
+ * unless the restrictions on use therein are violated and require payment to Broadleaf in which case
+ * the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
+ * shall apply.
+ * 
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
+ * #L%
+ */
 package org.broadleafcommerce.common.config;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.broadleafcommerce.common.web.dialect.BLCDialect;
 import org.broadleafcommerce.common.web.dialect.BroadleafAttributeModelVariableModifierProcessor;
 import org.broadleafcommerce.common.web.dialect.BroadleafAttributeModifierProcessor;
 import org.broadleafcommerce.common.web.dialect.BroadleafFormReplacementProcessor;
 import org.broadleafcommerce.common.web.dialect.BroadleafModelVariableModifierProcessor;
+import org.broadleafcommerce.common.web.dialect.BroadleafProcessor;
 import org.broadleafcommerce.common.web.dialect.BroadleafTagReplacementProcessor;
 import org.broadleafcommerce.common.web.dialect.BroadleafTagTextModifierProcessor;
 import org.broadleafcommerce.common.web.dialect.DelegatingBroadleafAttributeModelVariableModifierProcessor;
@@ -13,77 +33,73 @@ import org.broadleafcommerce.common.web.dialect.DelegatingBroadleafFormReplaceme
 import org.broadleafcommerce.common.web.dialect.DelegatingBroadleafModelVariableModifierProcessor;
 import org.broadleafcommerce.common.web.dialect.DelegatingBroadleafTagReplacementProcessor;
 import org.broadleafcommerce.common.web.dialect.DelegatingBroadleafTagTextModifierProcessor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.processor.IProcessor;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
+import javax.annotation.Resource;
+
 @Configuration
-@ComponentScan
 public class Thymeleaf2Config {
     
-    @Bean
-    public List<DelegatingBroadleafAttributeModelVariableModifierProcessor> getAttributeModelVariableModifiers(List<BroadleafAttributeModelVariableModifierProcessor> processors) {
-        List<DelegatingBroadleafAttributeModelVariableModifierProcessor> results = new ArrayList<>();
-        for (BroadleafAttributeModelVariableModifierProcessor processor : processors) {
-            results.add(new DelegatingBroadleafAttributeModelVariableModifierProcessor(processor.getName(), processor, processor.getPrecedence()));
-        }
-        return results;
-    }
+    @Resource
+    protected ApplicationContext applicationContext;
     
+    protected static final Log LOG = LogFactory.getLog(Thymeleaf2Config.class);
+            
     @Bean
-    public List<DelegatingBroadleafAttributeModifierProcessor> getAttributeModifiers(List<BroadleafAttributeModifierProcessor> processors) {
-        List<DelegatingBroadleafAttributeModifierProcessor> results = new ArrayList<>();
-        for (BroadleafAttributeModifierProcessor processor : processors) {
-            results.add(new DelegatingBroadleafAttributeModifierProcessor(processor.getName(), processor, processor.getPrecedence()));
-        }
-        return results;
-    }
-    
-    @Bean
-    public List<DelegatingBroadleafFormReplacementProcessor> getFormReplacers(List<BroadleafFormReplacementProcessor> processors) {
-        List<DelegatingBroadleafFormReplacementProcessor> results = new ArrayList<>();
-        for (BroadleafFormReplacementProcessor processor : processors) {
-            results.add(new DelegatingBroadleafFormReplacementProcessor(processor.getName(), processor, processor.getPrecedence()));
-        }
-        return results;
-    }
-    
-    @Bean
-    public List<DelegatingBroadleafTagReplacementProcessor> getTagReplacers(List<BroadleafTagReplacementProcessor> processors) {
-        List<DelegatingBroadleafTagReplacementProcessor> results = new ArrayList<>();
-        for (BroadleafTagReplacementProcessor processor : processors) {
-            results.add(new DelegatingBroadleafTagReplacementProcessor(processor.getName(), processor, processor.getPrecedence()));
-        }
-        return results;
-    }
-    
-    @Bean
-    public List<DelegatingBroadleafTagTextModifierProcessor> getTagTextModifier(List<BroadleafTagTextModifierProcessor> processors) {
-        List<DelegatingBroadleafTagTextModifierProcessor> results = new ArrayList<>();
-        for (BroadleafTagTextModifierProcessor processor : processors) {
-            results.add(new DelegatingBroadleafTagTextModifierProcessor(processor.getName(), processor, processor.getPrecedence()));
-        }
-        return results;
-    }
-    
-    @Bean
-    public List<DelegatingBroadleafModelVariableModifierProcessor> getModelVariableModifier(List<BroadleafModelVariableModifierProcessor> processors) {
-        List<DelegatingBroadleafModelVariableModifierProcessor> results = new ArrayList<>();
-        for (BroadleafModelVariableModifierProcessor processor : processors) {
-            results.add(new DelegatingBroadleafModelVariableModifierProcessor(processor.getName(), processor, processor.getPrecedence()));
-        }
-        return results;
-    }
-    
-    @Bean(name = "blDialect")
-    public BLCDialect getDialect(Set<IProcessor> processors) {
+    public BLCDialect blDialect() {
         BLCDialect dialect = new BLCDialect();
-        dialect.setProcessors(processors);
+        Set<IProcessor> iProcessors = new HashSet<>();
+        Collection<BroadleafProcessor> blcProcessors = applicationContext.getBeansOfType(BroadleafProcessor.class).values();
+        for (BroadleafProcessor proc : blcProcessors) {
+            if (BroadleafModelVariableModifierProcessor.class.isAssignableFrom(proc.getClass())) {
+                iProcessors.add(createDelegatingModelVariableModifierProcessor((BroadleafModelVariableModifierProcessor) proc));
+            } else if (BroadleafTagTextModifierProcessor.class.isAssignableFrom(proc.getClass())) {
+                iProcessors.add(createDelegatingTagTextModifierProcessor((BroadleafTagTextModifierProcessor) proc));
+            } else if (BroadleafTagReplacementProcessor.class.isAssignableFrom(proc.getClass())) {
+                iProcessors.add(createDelegatingTagReplacementProcessor((BroadleafTagReplacementProcessor) proc));
+            } else if (BroadleafFormReplacementProcessor.class.isAssignableFrom(proc.getClass())) {
+                iProcessors.add(createDelegatingFormReplacementProcessor((BroadleafFormReplacementProcessor) proc));
+            } else if (BroadleafAttributeModifierProcessor.class.isAssignableFrom(proc.getClass())) {
+                iProcessors.add(createDelegatingAttributeModifierProcessor((BroadleafAttributeModifierProcessor) proc));
+            } else if (BroadleafAttributeModelVariableModifierProcessor.class.isAssignableFrom(proc.getClass())) {
+                iProcessors.add(createDelegatingAttributeModelVariableModifierProcessor((BroadleafAttributeModelVariableModifierProcessor) proc));
+            } else {
+                LOG.warn("No known delegating processor to instantiate processor " + proc);
+            }
+        }
+        dialect.setProcessors(iProcessors);
         return dialect;
     }
+    
+    public DelegatingBroadleafModelVariableModifierProcessor createDelegatingModelVariableModifierProcessor(BroadleafModelVariableModifierProcessor processor) {
+        return new DelegatingBroadleafModelVariableModifierProcessor(processor.getName(), processor, processor.getPrecedence());
+    }
+    
+    public DelegatingBroadleafTagTextModifierProcessor createDelegatingTagTextModifierProcessor(BroadleafTagTextModifierProcessor processor) {
+        return new DelegatingBroadleafTagTextModifierProcessor(processor.getName(), processor, processor.getPrecedence());
+    }
+    
+    public DelegatingBroadleafTagReplacementProcessor createDelegatingTagReplacementProcessor(BroadleafTagReplacementProcessor processor) {
+        return new DelegatingBroadleafTagReplacementProcessor(processor.getName(), processor, processor.getPrecedence());
+    }
+    
+    public DelegatingBroadleafFormReplacementProcessor createDelegatingFormReplacementProcessor(BroadleafFormReplacementProcessor processor) {
+        return new DelegatingBroadleafFormReplacementProcessor(processor.getName(), processor, processor.getPrecedence());
+    }
+    
+    public DelegatingBroadleafAttributeModifierProcessor createDelegatingAttributeModifierProcessor(BroadleafAttributeModifierProcessor processor) {
+        return new DelegatingBroadleafAttributeModifierProcessor(processor.getName(), processor, processor.getPrecedence());
+    }
+
+    public DelegatingBroadleafAttributeModelVariableModifierProcessor createDelegatingAttributeModelVariableModifierProcessor(BroadleafAttributeModelVariableModifierProcessor processor) {
+        return new DelegatingBroadleafAttributeModelVariableModifierProcessor(processor.getName(), processor, processor.getPrecedence());
+    }
+    
 }
