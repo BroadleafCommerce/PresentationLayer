fa0ea574d548ddfc11d88f8db49c962d5434b2af
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2016-08-26 18:26:15 -0500

    Implemented BroadleafCacheProcessor for Thymeleaf 3 and removed extra files that were used to do the same thing in Thymeleaf 2 but aren't needed in Thymeleaf 3. Also added the java config so that the processor is added to the dialect. This has to be added separately since it doesn't extend any common layer stuff therefore the normal setup doesn't apply

--- broadleaf-thymeleaf3-presentation/src/main/java/org/broadleafcommerce/site/config/Thymeleaf3SiteConfig.java
+++ broadleaf-thymeleaf3-presentation/src/main/java/org/broadleafcommerce/site/config/Thymeleaf3SiteConfig.java
@@ -21,9 +21,11 @@ import org.broadleafcommerce.common.config.Thymeleaf3ConfigUtils;
 import org.broadleafcommerce.common.web.dialect.BLCDialect;
 import org.broadleafcommerce.common.web.dialect.BroadleafProcessor;
 import org.broadleafcommerce.common.web.resolver.BroadleafThymeleafTemplateResolver;
+import org.broadleafcommerce.thymeleaf.processor.BroadleafCacheProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import java.util.Collection;
@@ -41,7 +43,9 @@ public class Thymeleaf3SiteConfig {
     public BLCDialect blDialect() {
         BLCDialect dialect = new BLCDialect();
         Collection<BroadleafProcessor> blcProcessors = applicationContext.getBeansOfType(BroadleafProcessor.class).values();
-        dialect.setProcessors(Thymeleaf3ConfigUtils.getDialectProcessors(blcProcessors));
+        Set<IProcessor> iProcessors = Thymeleaf3ConfigUtils.getDialectProcessors(blcProcessors);
+        iProcessors.add(blCacheProcessor());
+        dialect.setProcessors(iProcessors);
         return dialect;
     }
     
@@ -55,6 +59,11 @@ public class Thymeleaf3SiteConfig {
     public Set<ITemplateResolver> blEmailTemplateResolvers() {
         Collection<BroadleafThymeleafTemplateResolver> resolvers = applicationContext.getBeansOfType(BroadleafThymeleafTemplateResolver.class).values();
         return Thymeleaf3ConfigUtils.getEmailResolvers(resolvers, applicationContext);
+    }
+    
+    @Bean
+    public IProcessor blCacheProcessor() {
+        return new BroadleafCacheProcessor();
     }
 
 }
