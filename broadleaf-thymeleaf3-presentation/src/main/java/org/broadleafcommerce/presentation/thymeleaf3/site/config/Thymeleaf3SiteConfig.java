bbe740499f0921a38c94db7f6187412ecb94513d
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2017-05-15 14:41:24 -0500

    Moved creation of blTemplateCacheKeyResolver to Java config so that it can be properly overridden by Enterprise's EnterpriseCacheKeyResolver

--- broadleaf-thymeleaf3-presentation/src/main/java/org/broadleafcommerce/presentation/thymeleaf3/site/config/Thymeleaf3SiteConfig.java
+++ broadleaf-thymeleaf3-presentation/src/main/java/org/broadleafcommerce/presentation/thymeleaf3/site/config/Thymeleaf3SiteConfig.java
@@ -17,6 +17,8 @@
  */
 package org.broadleafcommerce.presentation.thymeleaf3.site.config;
 
+import org.broadleafcommerce.presentation.cache.service.SimpleCacheKeyResolver;
+import org.broadleafcommerce.presentation.cache.service.TemplateCacheKeyResolverService;
 import org.broadleafcommerce.presentation.dialect.BroadleafProcessor;
 import org.broadleafcommerce.presentation.resolver.BroadleafTemplateResolver;
 import org.broadleafcommerce.presentation.thymeleaf3.config.Thymeleaf3ConfigUtils;
@@ -64,6 +66,11 @@ public class Thymeleaf3SiteConfig {
     @Bean
     public IProcessor blArbitraryHtmlInjectionProcessor() {
         return new ArbitraryHtmlInsertionProcessor();
+    }
+    
+    @Bean
+    public TemplateCacheKeyResolverService blTemplateCacheKeyResolver() {
+        return new SimpleCacheKeyResolver();
     }
     
     @Bean
