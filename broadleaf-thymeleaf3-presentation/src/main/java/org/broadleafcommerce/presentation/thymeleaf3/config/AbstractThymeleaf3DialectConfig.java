a445b37d7dc41e29c35d975f50f6517232af08c4
jaisenbrey <jaisenbrey@broadleafcommerce.com>
2019-10-04 17:06:53 -0500

    Update Thymeleaf Spring Security package name

--- broadleaf-thymeleaf3-presentation/src/main/java/org/broadleafcommerce/presentation/thymeleaf3/config/AbstractThymeleaf3DialectConfig.java
+++ broadleaf-thymeleaf3-presentation/src/main/java/org/broadleafcommerce/presentation/thymeleaf3/config/AbstractThymeleaf3DialectConfig.java
@@ -30,7 +30,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.dialect.IDialect;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.spring5.dialect.SpringStandardDialect;
 
